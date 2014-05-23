98a6847f2628d7572c0626718be702d7299f25a1
Steve Brewer <steve.brewer@gmail.com>
2014-05-23 08:10:26 -0500

    Works, exponentiallly slow, gonna have to fix that

--- lib/topcoder/eagle_in_zoo/node.rb
+++ lib/topcoder/eagle_in_zoo/node.rb
@@ -7,6 +7,7 @@ module EagleInZoo
       self.id = id
       self.children = []
       self.has_eagle = false
+      @cache = {}
     end
 
     # Takes a list of parent indexes
@@ -40,23 +41,36 @@ module EagleInZoo
       new_states = []
       fly_away = 0
       states.each do |state|
-        if state.include?(self.id)
-          if children.empty?
-            # end of the branch - eagle flies away
-            new_states << state.clone
-            fly_away = fly_away + 1
-          else
-            children.each do |child|
-              child_states, child_fly_aways = child.possible_future_states_given([state])
-              child_states.each{|new_state| new_states << new_state}
-              fly_away = fly_away + child_fly_aways
-            end
-          end
-        else
-          # this node isn't occupied, bird lands here, that's it
-          new_states << state.clone.add(self.id)
+        state.possible_future_state_given_state(state).tap do |s, f|
+          fly_away = fly_away + f
+          states = states + s
         end
       end
+      return new_states, fly_away
+    end
+
+    def possible_future_state_given_state state
+      return @cache[state] if @cache.try(:has_key?, state)
+
+      new_states = []
+      fly_away = 0
+      if state.include?(self.id)
+        if children.empty?
+          # end of the branch - eagle flies away
+          new_states << state.clone
+          fly_away = 1
+        else
+          children.each do |child|
+            child_states, child_fly_aways = child.possible_future_states_given_state(state)
+            child_states.each{|new_state| new_states << new_state}
+            fly_away = fly_away + child_fly_aways
+          end
+        end
+      else
+        # this node isn't occupied, bird lands here, that's it
+        new_states << state.clone.add(self.id)
+      end
+      @cache.try(:[]=, state, [new_states, fly_away])
       return new_states, fly_away
     end
   end
